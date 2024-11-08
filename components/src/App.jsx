a78b01029c56e20e13461f9e475ca7896d4084f9
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-11-08 20:38:44 +0800

    deep dive on learning reactjs

---
+++ components/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ModuleOne from "./pages/ModuleOne";
+
+function App() {
+  return (
+    <>
+      <ModuleOne />
+    </>
+  );
+}
+
+export default App;
---
+++ components/src/pages/ModuleOne.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ExerciseOne } from "../components/ExerciseOne";
+const ModuleOne = () => {
+  return (
+    <div>
+      <div>
+        <details>
+          <summary>
+            <strong>Module One</strong>
+          </summary>
+          <p>
+            <a href="">Exercise One</a>
+          </p>
+        </details>
+      </div>
+    </div>
+  );
+};
+
+export default ModuleOne;
